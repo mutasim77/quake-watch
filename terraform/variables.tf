985e3c38e5294e879d354ebc2e0a3556d9b39938
Mutasim  <96326525+mutasim77@users.noreply.github.com>
2024-05-15 16:07:57 +0500

    📜 fix: update initial vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,30 +1,30 @@
 variable "app_name" {
   type        = string
   description = "Application Name"
-  default     = "earthquakes"
+  default     = "name" # You application name
 }
 
 variable "project_id" {
   type        = string
   description = "The name of the project"
-  default     = "copper-axiom-326903"
+  default     = "id"  # You project id
 }
 
 variable "region" {
   type        = string
   description = "The default compute region"
-  default     = "us-east1"
+  default     = "" # Region
 }
 
 variable "zone" {
   type        = string
   description = "The default compute zone"
-  default     = "us-east1-c"
+  default     = "" # Zone
 }
 
 variable "credentials" {
   description = "key for credentials acess"
-  default     = "./keys/keys.json"
+  default     = "./keys/keys.json" # GPC Credentials
 }
 
 variable "storage_class" {
@@ -35,29 +35,29 @@ variable "storage_class" {
 variable "bq_dataset" {
   description = "BigQuery Dataset that data will be written to"
   type        = string
-  default     = "earthquake_dataset"
-}
+  default     = ""  # Name of BQ dataset
+} 
 
 variable "docker_image" {
   type        = string
   description = "The docker image to deploy to Cloud Run."
-  default     = "mageai/mageai:latest"
+  default     = "mageai/mageai:latest" # The name of image
 }
 
 variable "docker_image_tag" {
   type        = string
   description = "The docker image tag to deploy to Cloud Run."
-  default     = "mage-image"
+  default     = "tag" # tag for image
 }
 
 variable "docker_image_name" {
   type        = string
-  description = "The docker image tag to deploy to Cloud Run."
-  default     = "mageai"
+  description = "The docker image name to deploy to Cloud Run."
+  default     = "name" # name of image
 }
 
 variable "artifact_registry_path" {
   type        = string
   description = "The docker image tag to deploy to Cloud Run."
-  default     = "us-east1-docker.pkg.dev/copper-axiom-326903/quakewatch-repository"
+  default     = "" # Your Artifact Registry Path
 }
